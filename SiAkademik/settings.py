53fdc9ed06f895484ffa257ef99eb3e864a4d6b8
Muhammad Rovino Sanjaya <rovino.rs@gmail.com>
2023-06-19 12:51:28 +0700

    update mime checking

--- SiAkademik/settings.py
+++ SiAkademik/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".css", True)
+mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("application/javascript", ".js", True)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
